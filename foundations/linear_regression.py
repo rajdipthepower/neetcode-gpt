c6b3545ab5d93069c6eadd6b370564d1822f34a1
Rajdip Sinha <rajdipthepower360@gmail.com>
2026-09-06 16:52:01 +0530

    Update Linear regression forward pass (Math Foundations 5/6)

--- foundations/linear_regression.py
+++ foundations/linear_regression.py
@@ -11,4 +11,5 @@ class Solution:
     def get_error(self, model_prediction: NDArray[np.float64], ground_truth: NDArray[np.float64]) -> float:
         # Compute mean squared error between predictions and ground truth
         # Round to 5 decimal places
-        return np.round(np.mean((model_prediction-ground_truth)**2),5)
+        model_prediction -= ground_truth
+        return np.round(np.dot(model_prediction.reshape(-1), model_prediction.reshape(-1))/len(model_prediction),5)
